b5b27e15cdf093b373e766b36b6b52641b689f97
LozzDev <lozzdeveloper@gmail.com>
2025-02-18 11:12:37 +0100

    add game board setup and drawing functions for Tetris implementation

--- main.js
+++ main.js
@@ -8,3 +8,46 @@ const scoreElement = document.getElementById('score');
 
 const highScoreElement = document.getElementById('highScore');
 
+const ROWS = 20, COLS =10, SIZE = 30;
+
+let board = Array.from({length: ROWS}, () => Array(COLS).fill(0));
+
+let isPaused = false;
+let gameInterval; 
+let score = 0;
+let highScore = localStorage.getItem("highScore") || 0;
+
+highScoreElement.textContent = highScore;
+
+const SHAPES = [
+    [[1,1,1,1]], //palo
+    [[1,1],[1,1]], //cuadrado
+    [[0,1,0], [1,1,1]],//T
+    [[1,1,0], [0,1,1]], //Z
+    [[0,1,1],[1,1,0]]  //S
+];
+
+let currentPiece = {
+    shape: SHAPES[Math.floor(Math.random()*SHAPES.lenght)],
+    x: 3,
+    y: 0
+};
+
+function drawBoard(){
+    ctx.clearRect(0,0, canvas.Width, canvas.Height);
+    board.forEach((row, y) => row.forEach((value, x) => {
+        if(value) drawBlock(x,y, "cyan");
+    }));
+}
+
+
+
+
+function drawBlock(x, y, color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x * SIZE, y * SIZE, SIZE);
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(x * SIZE, y * SIZE, SIZE);
+}
+
+
